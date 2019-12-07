fa81a84e37b94f60fd936e9f7cb682046a3e0947
Usman Sheikh <sh.usmanikram@hotmail.com>
2019-12-08 01:01:12 +0500

    initial

---
+++ TestApp/TestApp/Data/TestAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApp.Models
+{
+    public class TestAppContext : DbContext
+    {
+        public TestAppContext (DbContextOptions<TestAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestApp.Models.Product> Product { get; set; }
+    }
+}
---
+++ TestApp/TestApp/Migrations/20191207195113_initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestApp.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
